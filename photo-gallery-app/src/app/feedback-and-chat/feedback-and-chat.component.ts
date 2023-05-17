3893c28f0bae2c80d871fd0ab332ebd3d20e9660
Regorlas <roger.random@outlook.com>
2023-05-17 03:12:10 +0000

    Feedback form logs to console

--- photo-gallery-app/src/app/feedback-and-chat/feedback-and-chat.component.ts
+++ photo-gallery-app/src/app/feedback-and-chat/feedback-and-chat.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-feedback-and-chat',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './feedback-and-chat.component.html',
   styleUrls: ['./feedback-and-chat.component.css']
 })
 export class FeedbackAndChatComponent {
+  private formBuilder = inject(FormBuilder);
 
+  contactForm = this.formBuilder.group({
+    fullname: '',
+    email: '',
+    comments: ''
+  });
+
+  showMsg: boolean = false;
+
+  onSubmit(): void {
+    console.log('Your feedback has been submitted', this.contactForm.value);
+    this.showMsg = true;
+    this.contactForm.reset();
+  }
 }
